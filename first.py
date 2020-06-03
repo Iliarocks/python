2bc322ad4a7df90597a04a018be6b08838aa2cc5
Ilia Parunashvili <iparunashvili123@gmail.com>
2020-06-03 14:05:08 -0400

    hi

---
+++ first.py
@@ -0,0 +1,15 @@
+def print_name(name):
+    if name:
+        print(name, 'is bad... psyche')
+
+print_name('Ilia')
+
+age = 13
+
+def get_dog_years(age):
+    if age < 2:
+        return 'Age must be over two to work'
+    elif age >= 2:
+        return str(24 + (age - 2) * 4)
+
+print(get_dog_years(age))
